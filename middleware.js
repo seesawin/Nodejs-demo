2d1554cd0915a06c89c8ca2aede3e32271a5ec1e
seesawin <seesawin@gmail.com>
2016-11-16 22:24:00 +0800

    20161116 update

--- middleware.js
+++ middleware.js
@@ -3,40 +3,54 @@ var url  = require('url');
 
 var routes = {'all' : []};
 var app = {};
-app.use = function(path, action) {
-	console.log('**************** app.use ****************');
-	var handle = {
-		path : pathRegexp(path),
-		stack : Array.prototype.slice.call(arguments, 1)
+app.use = function(path) {
+	console.log('**************** app.use start ****************');
+
+	var handle = {};
+	if(typeof path === 'string') {
+		handle = {
+			path : pathRegexp(path),
+			stack : Array.prototype.slice.call(arguments, 1)
+		}
+	} else {
+		handle = {
+			path : pathRegexp('/'),
+			stack : Array.prototype.slice.call(arguments, 0)
+		}
 	}
+	
 	routes.all.push(handle);
+	console.log('**************** app.use end ****************');
 }
 
 var match = function(pathname, routes) {
-	console.log('**************** match ****************');
+	console.log('**************** match start ****************');
+	console.log('length : ' + routes.length);
 
+	var stacks = [];
 	for(var i = 0; i < routes.length; i++) {
 		var route = routes[i];
 		var reg = route.path;
 		console.log(route);
-		console.log(reg);
+		console.log('reg : ' + reg);
 
 		var matched = reg.exec(pathname);
-		console.log(matched);
+		console.log('matched : ' + matched);
 
 		if(matched) {
-			// handle(req, res, route.stack);
-			return {
-				state :true,
-				stack : route.stack
-			};
+			console.log(55555555555555555555555555555555555);
+			stacks = stacks.concat(route.stack);
 		}
 	}
-	return false;
+	
+	console.log('obj stack lenth : ' + stacks.length );
+	console.log('**************** matched end **************** ');
+
+	return stacks;
 }
 
 var handle = function(req, res, stack) {
-	console.log('**************** handle ****************');
+	console.log('**************** handle start ****************');
 
 	var next = function() {
 		console.log('**************** next() 從stack取出中間件並執行 ****************');
@@ -51,6 +65,7 @@ var handle = function(req, res, stack) {
 	}
 	// 啟動
 	next();
+	console.log('**************** handle end ****************');
 }
 
 var pathRegexp = function(path) {
@@ -75,23 +90,25 @@ var pathRegexp = function(path) {
 }
 
 var done = function(req, res) {
-	console.log('**************** done ****************');
+	console.log('**************** done start ****************');
 	(function(req, res) {
 		console.log(req.query);
 		console.log('init server!');
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.end('測試中間件 OK!');
+		console.log('**************** done end ****************');
 	})(req, res);
 }
 
 var queryString = function(req, res, next) {
-	console.log('**************** queryString ****************');
+	console.log('**************** queryString start ****************');
 	req.query = url.parse(req.url, true).query;
 	next();
+	console.log('**************** queryString end ****************');
 }
 
 var cookie = function(req, res, next) {
-	console.log('**************** cookie ****************');
+	console.log('**************** cookie start ****************');
 	var cookie = req.headers.cookie;
 	var cookies = {};
 	if(cookie) {
@@ -103,6 +120,7 @@ var cookie = function(req, res, next) {
 	}
 	req.cookies = cookies;
 	next();
+	console.log('**************** cookie end ****************');
 }
 
 var serverMiddleware = function(req, res) {
@@ -110,19 +128,46 @@ var serverMiddleware = function(req, res) {
 	if (url.parse(req.url).pathname === '/favicon.ico') {
 		console.log('everything here is ignored');
 	} else {
-		app.use('/pathA/:username', queryString, cookie, done);
+		// 添加RESTful請求方法定義資源操作
+		['get', 'post', 'delete', 'put'].forEach(function(method){
+			routes[method] = [];
+			app[method] = function(path, action) {
+				routes[method].push({path : pathRegexp(path), stack : [action]});
+			}
+		})
+
+		// 添加MVC對應
+		app.use(queryString);
+		app.use(cookie);
+		app.get('/', done);
+		app.post('/user/:username', done);
+		app.delete('/user/:username', done);
+		app.put('/user/:username', done);
 
 		var pathname = url.parse(req.url).pathname;
-		console.log(pathname);
+		console.log('pathname : ' + pathname);
 
-		var result = match(pathname, routes.all);
-		if(result.state) {
-			console.log('result ok!');
-			handle(req, res, result.stack);
+		var method = req.method.toLowerCase();
+		console.log('method : ' + method);
+
+		var stack = match(pathname, routes.all);
+		console.log('001 stack.length =' + stack.length);
+
+		if(routes.hasOwnProperty(method)) {
+			console.log('result hasOwnProperty!');
+			stack = stack.concat(match(pathname, routes[method]));
 		} else {
 			console.log('req err!');
 			res.writeHead(400, {'Content-Type': 'text/plain'});
 			res.end('系統錯誤!');
+		}
+
+		console.log('002 stack.length =' + stack.length);
+		if(stack.length) {
+			console.log('001');
+			handle(req, res, stack);
+		} else {
+			console.log('002');
 		}
 	}
 }
